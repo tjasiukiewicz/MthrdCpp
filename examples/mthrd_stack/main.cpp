de8b5c6afc97318f28f25dbf91229d06a3d1ba7f
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2025-02-19 14:10:50 +0100

    thstack solution

--- examples/mthrd_stack/main.cpp
+++ examples/mthrd_stack/main.cpp
@@ -4,7 +4,6 @@
 #include <thread>
 #include <memory>
 #include <chrono>
-#include <optional>
 
 template<typename T>
 class ThStack {
@@ -13,7 +12,12 @@ public:
 		: data{}, stop{false} {}
 
 	~ThStack() {
-		stop = true; // !!!
+		doStop();
+	}
+
+	void doStop() {
+		std::lock_guard<std::mutex> _(mtx);
+		stop = true;
 	}
 
 	bool push(T val) {
@@ -26,29 +30,6 @@ public:
 		}
 		return true;
 	}
-	/*
-	bool push(T & val) {
-		{
-			std::lock_guard<std::mutex> _(mtx);
-			if (stop) {
-				return false;
-			}
-			data.push(val);
-		}
-		return true;
-	}
-
-	bool push(T && val) {
-		{
-			std::lock_guard<std::mutex> _(mtx);
-			if (stop) {
-				return false;
-			}
-			data.push(val);
-		}
-		return true;
-	}
-	*/
 
 	bool pop(T & val) {
 		if (std::lock_guard<std::mutex> _(mtx); (not stop) or (not data.empty())) {
@@ -69,14 +50,10 @@ public:
 		return {};
 	}
 
-
-
 	std::size_t size() const {
 		std::lock_guard<std::mutex> _(mtx);
 		return data.size();
 	}
-
-
 
 private:
 	std::stack<T> data;
@@ -87,19 +64,21 @@ private:
 void producer(ThStack<int>& th) {
 	for (auto i = 0; i < 10; ++i) {
 		th.push(i);
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
 
 void consumer(ThStack<int>& th) {
-	for (auto i = 0; i < 5; ++i) {
+	for (auto i = 0; i < 10; ++i) {
 		std::shared_ptr<int> sp;
-		do {
-			sp = th.pop();
-			if (sp) {
-				std::cout << "comumer: " << *sp << '\n';
-			}
-		} while (not sp);
+		sp = th.pop();
+		std::cout << "consumer: ";
+		if (sp) {
+			std::cout << *sp << '\n';
+		} else {
+			std::cout << "empty" << '\n';
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 }
 
@@ -109,6 +88,8 @@ int main() {
 	auto cth1 = std::thread(consumer, std::ref(th));
 	auto cth2 = std::thread(consumer, std::ref(th));
 
+	std::this_thread::sleep_for(std::chrono::milliseconds(700));
+	th.doStop();
 
 	cth2.join();
 	cth1.join();
