ddecf6b7550ac20049dec98906dd0b855d3020a9
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2025-02-19 10:14:19 +0100

    var templates

--- examples/var_template/main.cpp
+++ examples/var_template/main.cpp
@@ -16,13 +16,32 @@ void show_args(T a, Args... args) {
 	show_args(args...);
 }
 
+template<typename Func, typename... Args> 
+void executor(Func func, Args... args) {
+	std::cout << "Exec func\n";
+	func(std::forward<Args>(args)...);
+}
+
+void greet(int && a, const char * msg) {
+	std::cout << a << ' ' << msg << '\n';
+}
+
+/*
+template<typename... Parents>
+class X: public Parents... {
+};
+*/
+
 
 int main() {
+	int var = 42;
 	/*
 	foo(1, 2, 3.14, "ala");
 	foo("ble", 'a', 43, 34, 434, 434);
 	*/
 
-	show_args(1, 3, "Adam", 12.434, 656.34F);
+	//show_args(1, 3, "Adam", 12.434, 656.34F);
+	
+	executor(greet, std::move(var), "word");
 
 }
