6deb02146fcdb21e65e2d3b12145f0ce167f6172
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2025-02-20 13:19:01 +0100

    naked

--- examples/naked_queue/main.cpp
+++ examples/naked_queue/main.cpp
@@ -10,20 +10,30 @@ std::queue<int> data_queue;
 std::condition_variable cv;
 
 void sender(int data) {
-	// TODO: Push data to data_queue
+	std::lock_guard<std::mutex> _(mtx);
+	data_queue.push(data);
+	cv.notify_one();
 }
 
 void receiver(const std::string& name) {
 	for (;;) {
-		// TODO : Get front data from data_queue
+		std::unique_lock<std::mutex> lk(mtx);
+		cv.wait(lk, [] { return not data_queue.empty(); });
+		auto data = data_queue.front();
+		data_queue.pop();
+		lk.unlock();
+		std::cout << "Worker " << name << " data = " << data << std::endl;
 	}
 }
 
 int main() {
-	std::thread t1(worker, "First");
-	std::thread t2(worker, "Second");
+	std::thread t1(receiver, "First");
+	std::thread t2(receiver, "Second");
 	for (auto i = 0U; i < 10; ++i) {
 		sender(i * 100);
 		//std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
+
+	t2.join();
+	t1.join();
 }
