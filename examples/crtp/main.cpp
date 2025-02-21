e1fbf809d1d7dbd485f633f54ff2237d1eccf6a8
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2025-02-21 10:51:13 +0100

    CRTP

--- examples/crtp/main.cpp
+++ examples/crtp/main.cpp
@@ -2,10 +2,30 @@
 #include <vector>
 #include <string>
 
-class Airplane {
+// CRTP
+
+template<typename T>
+class InstancesRegister {
+public:
+	InstancesRegister(T * t) {
+		InstancesRegister<T>::instances.emplace_back(t);
+	}
+
+	static std::vector<T*>& getInstances() {
+		return InstancesRegister::instances;
+	}
+private:
+	static std::vector<T*> instances;
+};
+
+template<typename T>
+std::vector<T*> InstancesRegister<T>::instances;
+
+class Airplane: public InstancesRegister<Airplane> { /// <--- CRTP: class X: public Y<X> { ... 
 public:
 	explicit Airplane(const std::string& name_)
-		: name{name_} {}
+		: InstancesRegister(this), name{name_} {
+	}
 	void info() const {
 		std::cout << "Airplane: " << name << '\n';
 	}
@@ -15,9 +35,16 @@ private:
 
 int main() {
 	Airplane a1("F35");
-	Airplane a2("F15");
+	{
+		Airplane a2("F15");
+	}
 
+	/*
 	for (auto a: {&a1, &a2}) {
+		a->info();
+	}
+	*/
+	for (auto a: Airplane::getInstances()) {
 		a->info();
 	}
 }
